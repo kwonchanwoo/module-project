b9ddd89e2461e87ce1cc51121395503d8ce111d8
권창우 <cukown@jeit.co.kr>
2023-08-16 10:56:56 +0900

    로그인 - 로그인시 대소문자 구분 없도록 변경

--- user/src/main/java/com/example/module/authorize/service/AuthorizeService.java
+++ user/src/main/java/com/example/module/authorize/service/AuthorizeService.java
@@ -52,7 +52,7 @@ public class AuthorizeService implements UserDetailsService {
             // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
             // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    loginDto.getEmail(), loginDto.getPassword()
+                    loginDto.getEmail().toLowerCase(), loginDto.getPassword().toLowerCase()
             );
 
             // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
